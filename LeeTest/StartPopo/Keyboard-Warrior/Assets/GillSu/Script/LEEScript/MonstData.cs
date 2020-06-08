2fb94d406eeaa8f942fa27ffc48aa8b9e5f287c4
LeeGilsu <lgs731@naver.com>
2020-06-08 22:45:06 +0900

    dt

---
+++ LeeTest/StartPopo/Keyboard-Warrior/Assets/GillSu/Script/LEEScript/MonstData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Mobster Data", menuName = "Scriptable Object/Monster Data", order = int.MaxValue)]
+public class MonstData : ScriptableObject // 씬에있는 오브젝트를 붙일 수 없다.
+    //사용법
+{
+    public string Name;
+    public int HP;
+    public float moveSpeed;
+}
